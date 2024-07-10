7dcd19377ffc3a72b4db77a779f178106601533c
MdArifAnsari01 <130689750+MdArifAnsari01@users.noreply.github.com>
2024-07-10 20:27:28 +0530

    Update ec2-resources.tf

--- ec2-resources.tf
+++ ec2-resources.tf
@@ -1,10 +1,11 @@
 # Public subnet EC2 instance 1
+# Make sure to put your own key and ami
 resource "aws_instance" "two-tier-web-server-1" {
-  ami             = "ami-064eb0bee0c5402c5"
+  ami             = "ami-06c68f701d8090592"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.two-tier-ec2-sg.id]
   subnet_id       = aws_subnet.two-tier-pub-sub-1.id
-  key_name   = "two-tier-key"
+  key_name   = "testKP"
 
   tags = {
     Name = "two-tier-web-server-1"
@@ -13,19 +14,20 @@ resource "aws_instance" "two-tier-web-server-1" {
   user_data = <<-EOF
 #!/bin/bash
 sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y 
-sudo systemctl enable nginx
-sudo systemctl start nginx
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
 EOF
 }
 
 # Public subnet  EC2 instance 2
+# Make sure to put your own key and ami
 resource "aws_instance" "two-tier-web-server-2" {
-  ami             = "ami-064eb0bee0c5402c5"
+  ami             = "ami-06c68f701d8090592"
   instance_type   = "t2.micro"
   security_groups = [aws_security_group.two-tier-ec2-sg.id]
   subnet_id       = aws_subnet.two-tier-pub-sub-2.id
-  key_name   = "two-tier-key"
+  key_name   = "testKP"
 
   tags = {
     Name = "two-tier-web-server-2"
@@ -34,24 +36,8 @@ resource "aws_instance" "two-tier-web-server-2" {
   user_data = <<-EOF
 #!/bin/bash
 sudo yum update -y
-sudo amazon-linux-extras install nginx1 -y 
-sudo systemctl enable nginx
-sudo systemctl start nginx
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
 EOF
-}
-
-#EIP
-
-resource "aws_eip" "two-tier-web-server-1-eip" {
-  vpc = true
-
-  instance                  = aws_instance.two-tier-web-server-1.id
-  depends_on                = [aws_internet_gateway.two-tier-igw]
-}
-
-resource "aws_eip" "two-tier-web-server-2-eip" {
-  vpc = true
-
-  instance                  = aws_instance.two-tier-web-server-2.id
-  depends_on                = [aws_internet_gateway.two-tier-igw]
 }
